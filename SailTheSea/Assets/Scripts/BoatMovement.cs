a8e27fd8d7a5140fe834391ec0cd11c2f103aaa6
Lempi24 <78370981+Lempi24@users.noreply.github.com>
2023-05-30 13:54:08 +0200

    Naprawa popierdolonej diagonalnej prędkości

--- SailTheSea/Assets/Scripts/BoatMovement.cs
+++ SailTheSea/Assets/Scripts/BoatMovement.cs
@@ -14,10 +14,11 @@ public class BoatMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat("Speed", movement.magnitude);
     }
     void FixedUpdate()
     {
