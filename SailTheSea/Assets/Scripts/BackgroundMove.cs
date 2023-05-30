b8683c1bd65a4a2b2f48c0b5e5c8a01e829ca55a
oswitek <oswitek@edu.cdv.pl>
2023-05-30 23:33:54 +0200

    tło przyspiesza i sie loopuje

---
+++ SailTheSea/Assets/Scripts/BackgroundMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMove : MonoBehaviour
+{
+    public float speed = 4f, time = 0f;
+    private Vector3 StartPosition;
+
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        time += Time.deltaTime;
+        if (transform.position.y < -6.70512f)
+        {
+            StartPosition.y = 6.95f;
+            transform.position = StartPosition;
+        }
+        if (time > 30f)
+        {
+            speed += 0.2f;
+            time = 0f;
+        }
+    }
+}
