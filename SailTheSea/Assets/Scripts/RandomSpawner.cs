762e1b49f7cef512bdae07fcc3b185ddd32d35ae
Lempi24 <ShadowHunter69@wp.pl>
2023-05-31 21:22:42 +0200

    Spawner added
    Będzie trzeba poprawić tajming spawnów na więcej niż na jedno miejsce albo zwiększyć częstotliwość coś się potem ogarnie

---
+++ SailTheSea/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] spawnPrefabs;
+    public float spawnInterval = 1f; // Czas miêdzy kolejnymi spawnami
+
+    private float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            timer = 0f;
+            SpawnRandomPrefab();
+        }
+    }
+
+    void SpawnRandomPrefab()
+    {
+        int randomIndex = Random.Range(0, spawnPrefabs.Length);
+        int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(spawnPrefabs[randomIndex], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+    }
+}
