9ce0bb647d2d31a80cff1425e2ec2b99e8e5a683
oswitek <oswitek@edu.cdv.pl>
2023-06-01 00:28:16 +0200

    added Dash LeftShift duration : 1s cooldown: 10s

--- SailTheSea/Assets/Scripts/BoatMovement.cs
+++ SailTheSea/Assets/Scripts/BoatMovement.cs
@@ -8,10 +8,24 @@ public class BoatMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
 
+    //Dashing
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 200f;
+    private float dashingTime = 1f;
+    private float dashingCooldown = 10f;
+
+    [SerializeField] private TrailRenderer tr;
+
     Vector2 movement;
-    // Update is called once per frame
+    
     void Update()
     {
+        if (isDashing) 
+        {
+            return;
+        }
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement.Normalize();
@@ -19,10 +33,31 @@ public class BoatMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.magnitude);
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash == true)
+        {
+            StartCoroutine(Dash());
+        }
     }
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        tr.emitting = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
 }
