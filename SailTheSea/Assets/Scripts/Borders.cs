0040d89403361003c8f24f103e3a0e8262c6de1a
Lempi24 <ShadowHunter69@wp.pl>
2023-05-31 20:26:47 +0200

    Object falling and destroy on -y axis

--- SailTheSea/Assets/Scripts/Borders.cs
+++ SailTheSea/Assets/Scripts/Borders.cs
@@ -8,7 +8,7 @@ public class Borders : MonoBehaviour
 
     private void Start()
     {
-        // Obliczanie granic ekranu gry w jednostkach jednostki widoku
+        
         Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         minX = -screenBounds.x + 0.3f;
         maxX = screenBounds.x - 0.3f;
@@ -18,14 +18,14 @@ public class Borders : MonoBehaviour
 
     private void LateUpdate()
     {
-        // Pobieranie aktualnej pozycji gracza
+        
         Vector3 currentPosition = transform.position;
 
-        // Sprawdzanie, czy pozycja gracza przekracza granice ekranu gry
+       
         float clampedX = Mathf.Clamp(currentPosition.x, minX, maxX);
         float clampedY = Mathf.Clamp(currentPosition.y, minY, maxY);
 
-        // Aktualizowanie pozycji gracza z uwzglêdnieniem granic
+        
         transform.position = new Vector3(clampedX, clampedY, currentPosition.z);
     }
 }
