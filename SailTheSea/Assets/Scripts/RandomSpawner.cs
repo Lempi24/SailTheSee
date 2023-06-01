279b30aa49cb03925a724beb5e417ddcdeb61d77
Lempi24 <ShadowHunter69@wp.pl>
2023-06-01 14:52:02 +0200

    Particle system part 1

--- SailTheSea/Assets/Scripts/RandomSpawner.cs
+++ SailTheSea/Assets/Scripts/RandomSpawner.cs
@@ -6,26 +6,41 @@ public class RandomSpawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject[] spawnPrefabs;
-    public float spawnInterval = 1f; 
-
+    public float spawnInterval = 1f;
+    public int spawnNumber = 5;
     private float timer = 0f;
 
     void Update()
     {
         timer += Time.deltaTime;
 
-        if (timer >= spawnInterval)
+        if (timer >= spawnInterval && spawnPoints.Length >= 3)
         {
             timer = 0f;
-            SpawnRandomPrefab();
+            SpawnRandomPrefabs(spawnNumber);
         }
     }
 
-    void SpawnRandomPrefab()
+    void SpawnRandomPrefabs(int count)
     {
-        int randomIndex = Random.Range(0, spawnPrefabs.Length);
-        int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+        
+        List<int> availableSpawnIndexes = new List<int>();
 
-        Instantiate(spawnPrefabs[randomIndex], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            availableSpawnIndexes.Add(i);
+        }
+
+        List<int> spawnIndexes = new List<int>();
+
+        while (spawnIndexes.Count < count && availableSpawnIndexes.Count > 0)
+        {
+            int randomIndex = Random.Range(0, availableSpawnIndexes.Count);
+            int spawnIndex = availableSpawnIndexes[randomIndex];
+            availableSpawnIndexes.RemoveAt(randomIndex);
+
+            spawnIndexes.Add(spawnIndex);
+            Instantiate(spawnPrefabs[Random.Range(0, spawnPrefabs.Length)], spawnPoints[spawnIndex].position, Quaternion.identity);
+        }
     }
 }
