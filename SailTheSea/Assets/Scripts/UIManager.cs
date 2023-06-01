b61920c02024b772f6902c636620cbb9b3af8fd5
oswitek <oswitek@edu.cdv.pl>
2023-06-01 13:12:25 +0200

    GameOverMenu

---
+++ SailTheSea/Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject gameOverMenu;
+
+    private void OnEnable()
+    {
+        PlayerHealth.OnPlayerDeath += EnableGameOverMenu;
+    }
+
+    private void OnDisable()
+    {
+        PlayerHealth.OnPlayerDeath -= EnableGameOverMenu;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void EnableGameOverMenu()
+    {
+        gameOverMenu.SetActive(true);
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
