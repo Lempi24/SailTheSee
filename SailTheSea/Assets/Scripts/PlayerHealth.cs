75fed8f67baa3f680d593049929358cc48a7e5d0
oswitek <oswitek@edu.cdv.pl>
2023-06-08 03:30:06 +0200

    DamageCooldown zeby uniknac podwojnego dipsa

--- SailTheSea/Assets/Scripts/PlayerHealth.cs
+++ SailTheSea/Assets/Scripts/PlayerHealth.cs
@@ -9,17 +9,25 @@ public class PlayerHealth : MonoBehaviour
     public static event Action OnPlayerDeath;
 
     public float health, maxHealth;
-
+    public float DamageCooldown = 2f;
     void Start()
     {
         health = maxHealth;    
     }
-    
+
+    private void Update()
+    {
+        DamageCooldown -= Time.deltaTime;
+    }
     public void TakeDamage(float amount)
     {
-        health -= amount;
-        OnPlayerDamaged?.Invoke();
-
+        if (DamageCooldown <= 0f)
+        {
+            health -= amount;
+            OnPlayerDamaged?.Invoke();
+            DamageCooldown = 2f;
+        }
+        
         if(health <=0)
         {
             health = 0;
