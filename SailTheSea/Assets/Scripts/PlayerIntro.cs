f3304a34b11492b058de9475d4bc343620735d40
oswitek <oswitek@edu.cdv.pl>
2023-06-08 02:51:51 +0200

    hotfix

--- SailTheSea/Assets/Scripts/PlayerIntro.cs
+++ SailTheSea/Assets/Scripts/PlayerIntro.cs
@@ -7,11 +7,18 @@ public class PlayerIntro : MonoBehaviour
     [SerializeField] public GameObject Player;
     [SerializeField] public GameObject Position1;
     int i = 0;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
 
     void Update()
     {
         if (i < 1)
         {
+            rb.gravityScale = 0f;
             if (Player.transform.position.y != Position1.transform.position.y)
             {
                 transform.position = Vector2.MoveTowards(transform.position, Position1.transform.position, speed * Time.deltaTime);
@@ -20,6 +27,10 @@ public class PlayerIntro : MonoBehaviour
             {
                 i++;
             }
+        }
+        if (i > 0)
+        {
+            rb.gravityScale = 2f;
         }
     }
 }
