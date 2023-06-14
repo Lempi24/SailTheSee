9541dd21aeebebafabd865b49e9764077496d4e4
Lempi24 <ShadowHunter69@wp.pl>
2023-06-14 22:30:27 +0200

    Chuj wie co to się dzieje

--- SailTheSea/Assets/Scripts/ObstaclesBehavior.cs
+++ SailTheSea/Assets/Scripts/ObstaclesBehavior.cs
@@ -7,23 +7,28 @@ public class ObstaclesBehavior : MonoBehaviour
 
     private Camera mainCamera;
     public float speed = 5f;
-    private Rigidbody2D rb;
 
     private void Start()
     {
         mainCamera = Camera.main;
-        rb = this.GetComponent<Rigidbody2D>();
-        rb.velocity = new Vector2 (0, -speed);
     }
 
     private void Update()
     {
-        
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
         if (transform.position.y < mainCamera.transform.position.y - mainCamera.orthographicSize)
         {
             Destroy(gameObject);
         }
-        
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision, true);
+        }
+    }
+
 }
