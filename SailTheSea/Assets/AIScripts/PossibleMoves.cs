96a5f06ff7568642e4603bedb99429daae7cc243
Lempi24 <ShadowHunter69@wp.pl>
2023-06-21 14:41:49 +0200

    PlayerCurrentIndex working

--- SailTheSea/Assets/AIScripts/PossibleMoves.cs
+++ SailTheSea/Assets/AIScripts/PossibleMoves.cs
@@ -7,11 +7,12 @@ public class PossibleMoves : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject playerPrefab;
 
+    public int spawnIndex;
     private void Start()
     {
         
         int randomSpawnIndex = Random.Range(0, spawnPoints.Length);
-
+        spawnIndex = randomSpawnIndex;
         
         Transform randomSpawnPoint = spawnPoints[randomSpawnIndex];
 
@@ -23,5 +24,9 @@ public class PossibleMoves : MonoBehaviour
     {
         playerPrefab.transform.position = spawnPoint.position;
         playerPrefab.transform.rotation = spawnPoint.rotation;
+    }
+    public int GetSpawnIndex()
+    {
+        return spawnIndex;
     }
 }
--- SailTheSea/Assets/AIScripts/Raycast.cs
+++ SailTheSea/Assets/AIScripts/Raycast.cs
@@ -8,27 +8,35 @@ public class Raycast : MonoBehaviour
     public LayerMask layerMask;
     public Color rayColor = Color.red;
 
+    private PossibleMoves possibleMoves;
+
+    private void Start()
+    {
+        possibleMoves = FindObjectOfType<PossibleMoves>();
+    }
     private void Update()
     {
         Vector2 raycastOrigin = transform.position;
 
-        // Wykonywanie raycasta w prawo
+        
         Vector2 raycastDirectionRight = transform.right;
         RaycastHit2D hitRight = Physics2D.Raycast(raycastOrigin, raycastDirectionRight, raycastDistance, layerMask);
         if (hitRight.collider != null && hitRight.collider.CompareTag("Player"))
         {
-            Debug.Log("Gracz zosta³ dotkniêty!");
+            int playerCurrentIndex = possibleMoves.spawnIndex;
+            Debug.Log(playerCurrentIndex);
         }
 
-        // Wykonywanie raycasta w lewo
+       
         Vector2 raycastDirectionLeft = -transform.right;
         RaycastHit2D hitLeft = Physics2D.Raycast(raycastOrigin, raycastDirectionLeft, raycastDistance, layerMask);
         if (hitLeft.collider != null && hitLeft.collider.CompareTag("Player"))
         {
-            Debug.Log("Gracz zosta³ dotkniêty!");
+            int playerCurrentIndex = possibleMoves.spawnIndex;
+            Debug.Log(playerCurrentIndex);
         }
 
-        // Rysowanie linii raycastów
+        
         Debug.DrawLine(raycastOrigin, raycastOrigin + raycastDirectionRight * raycastDistance, rayColor);
         Debug.DrawLine(raycastOrigin, raycastOrigin + raycastDirectionLeft * raycastDistance, rayColor);
     }
