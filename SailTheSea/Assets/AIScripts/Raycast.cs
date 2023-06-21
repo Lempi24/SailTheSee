a6762d4e814055999f625fc40bc562eec0a23f30
Lempi24 <ShadowHunter69@wp.pl>
2023-06-21 14:52:46 +0200

    Zapis do pliku zewnętrznego

--- SailTheSea/Assets/AIScripts/Raycast.cs
+++ SailTheSea/Assets/AIScripts/Raycast.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Raycast : MonoBehaviour
 {
@@ -14,32 +15,47 @@ public class Raycast : MonoBehaviour
     {
         possibleMoves = FindObjectOfType<PossibleMoves>();
     }
+
     private void Update()
     {
         Vector2 raycastOrigin = transform.position;
 
-        
         Vector2 raycastDirectionRight = transform.right;
         RaycastHit2D hitRight = Physics2D.Raycast(raycastOrigin, raycastDirectionRight, raycastDistance, layerMask);
         if (hitRight.collider != null && hitRight.collider.CompareTag("Player"))
         {
             int playerCurrentIndex = possibleMoves.spawnIndex;
             Debug.Log(playerCurrentIndex);
+            SavePlayerCurrentIndex(playerCurrentIndex);
         }
 
-       
         Vector2 raycastDirectionLeft = -transform.right;
         RaycastHit2D hitLeft = Physics2D.Raycast(raycastOrigin, raycastDirectionLeft, raycastDistance, layerMask);
         if (hitLeft.collider != null && hitLeft.collider.CompareTag("Player"))
         {
             int playerCurrentIndex = possibleMoves.spawnIndex;
             Debug.Log(playerCurrentIndex);
+            SavePlayerCurrentIndex(playerCurrentIndex);
         }
 
-        
         Debug.DrawLine(raycastOrigin, raycastOrigin + raycastDirectionRight * raycastDistance, rayColor);
         Debug.DrawLine(raycastOrigin, raycastOrigin + raycastDirectionLeft * raycastDistance, rayColor);
     }
 
+    private void SavePlayerCurrentIndex(int index)
+    {
+        string filePath = "PlayerCurrentIndex.txt";
+
+        
+        StreamWriter writer = new StreamWriter(filePath, false);
+
+       
+        writer.WriteLine(index);
+
+       
+        writer.Close();
+
+        Debug.Log("PlayerCurrentIndex saved to file.");
+    }
 
 }
